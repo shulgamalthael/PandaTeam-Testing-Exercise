6b326f2a0475638c177a1d9024b55d7bf96d92c3
Nikita Shulha <shulgamalthael@gmail.com>
2023-12-17 10:56:30 +0200

    change openweather api protocol from http to https

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,6 +1,6 @@
 export default {
 	API_KEY: "24629a8e1d52d54a6084e06f4623facf",
-	BASE_API_URL: "http://api.openweathermap.org",
+	BASE_API_URL: "https://api.openweathermap.org",
 	WEATHER_API_URL: "https://api.weatherapi.com",
 	WEATHER_API_KEY: "ed6e4cc2d5fa4f52aa901144231412",
 	IP_API_KEY: '3d1a525830c1d37d38565af2f7edba114e0bf29ad29863829e378ba9',
